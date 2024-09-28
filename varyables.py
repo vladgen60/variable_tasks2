ced6b9578a5a13ed2ba6465c1312376992ee6699
vladgen60 <153118575+vladgen60@users.noreply.github.com>
2024-09-28 20:52:02 +0300

    Add files via upload

---
+++ varyables.py
@@ -0,0 +1,6 @@
+quantity_homeworks = 12
+quantity_hours = 1.5
+name_cours = 'Python'
+quantity_one_tasks = quantity_hours/quantity_homeworks
+
+print('Курс: ' + name_cours + ', всего задач: '  + str( quantity_homeworks) + ', затрачено часов: '+ str(quantity_hours) + ', среднее время выполнения: ' + str(quantity_one_tasks)+'.')
