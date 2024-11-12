3916c3d2338ec7c6f4a64f442411a6f1e71e7ebb
Vladimir <153118575+vladgen60@users.noreply.github.com>
2024-11-12 10:13:37 +0300

    Add files via upload

---
+++ for experiments.py
@@ -0,0 +1,23 @@
+class User:
+    """ «Класс пользователя, содержащий атрибуты: 
+    логин и пароль» 
+    """
+    def __init__(self, username, password, password_confirm):
+        self.username = username
+        if password == password_confirm:
+            self.password = password
+
+class Database:
+    def __init__(self):
+        self.data = {}
+
+    def add_user(self, username, password):
+        self.data[username] = password
+
+if __name__ == '__main__':
+    database = Database()
+    user = User(input('Введите логин: '), input('Введите пароль: '), input('Повторите пароль:'))
+    database.add_user(user.username, user.password)
+print(database.data)
+        
+        
