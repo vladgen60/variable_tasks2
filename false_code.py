406301d97d1daec1efa21452bb03033dafbaa1b9
Vladimir <153118575+vladgen60@users.noreply.github.com>
2025-02-05 17:29:41 +0300

    Add files via upload

---
+++ false_code.py
@@ -0,0 +1,12 @@
+my_list = [10, 20.3, 10, '50', [8, 0, 4]]
+# my_list.pop(2)
+print(my_list)
+d = my_list.count(10)
+print(d)
+print(len(my_list))
+
+print(my_list[::-1])
+my_num = ['a', 'b']
+my_list = my_list + my_num
+print(my_list)
+
