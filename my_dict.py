e1cc606507dba461d5f9cf97b6ddbd06a65723bc
Vladimir <153118575+vladgen60@users.noreply.github.com>
2025-02-09 15:34:49 +0300

    Add files via upload

---
+++ my_dict.py
@@ -0,0 +1,23 @@
+my_dict = {}
+d = input('Введите ключ в словарь:')
+c = input('Введите значение ключа:')
+my_dict[d] = c
+print(my_dict)
+a = input('Введите ключ в словарь:')
+b = input('Введите значение ключа:')
+my_dict[a] = b
+print(my_dict)
+q = input('Введите ключ в словарь:')
+w = input('Введите значение ключа:')
+my_dict[q] = w
+print(my_dict)
+
+
+
+
+
+
+
+
+
+
