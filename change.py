d643246675c80b954f96bb6db5871ce76fd8d424
Vladimir <153118575+vladgen60@users.noreply.github.com>
2025-02-14 16:35:29 +0300

    Add files via upload

---
+++ change.py
@@ -0,0 +1,23 @@
+ch = float(input('Введите сумму сдачи: '))
+tonne = ch // 2
+tonne1 = ch % 2
+loony = int(tonne1)
+tw_five = (tonne1)//0.25
+tw_five1 = (tonne1) %  0.25
+ten = tw_five1 // 0.1
+ten1 = tw_five1 % 0.1
+five = ten1 // 0.05 
+fiv1 = ten1 % 0.05
+one = fiv1 / 0.01
+
+
+
+
+print(tonne)
+print(loony)
+print(tw_five)
+print(ten)
+
+print(five)
+
+print(one)
