b6ccdf589d89701983fcf14aba04345d3bb77073
Vladimir <153118575+vladgen60@users.noreply.github.com>
2025-02-21 12:13:26 +0300

    Add files via upload

--- spreedsheet.py
+++ spreedsheet.py
@@ -1,8 +1,13 @@
-price_low = float(input('Введите цену со скидкой: '))
-price_list = []
-price_list.append(price_low)
-for i in range(len(price_list)):
-    high_price = price_low * 100 / 60
-    print('Старая цена: {0:5.2f}'.format(high_price))
-    print('Новая цена: {0:6.2f}'.format(price_low))
-    print(f'Ваша выгода: {(high_price - price_low):5.2f}')
+
+price_list = [4.95, 9.95, 14.95, 19.95, 24.95]
+n = 1
+
+for i in range(len(price_list) + 1):
+    high_price = price_list[i] * 100 / 60
+    print(f'Товар №{n}')
+    print('Старая цена: {0:5.2f}'.format(high_price) + '$')
+    print('Новая цена: {0:6.2f}'.format(price_list[i]) + '$')
+    print(f'Ваша выгода: {(high_price - price_list[i]):5.2f}' + '$')
+    print()
+    
+    n += 1
